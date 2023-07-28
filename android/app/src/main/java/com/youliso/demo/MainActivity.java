7c9ce271a0ea2e897bc7a1061183a40f72effd6a
youli <youliso@hotmail.com>
2023-07-28 13:48:54 +0800

    add

--- android/app/src/main/java/com/youliso/demo/MainActivity.java
+++ android/app/src/main/java/com/youliso/demo/MainActivity.java
@@ -2,4 +2,10 @@ package com.youliso.demo;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(EchoPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
